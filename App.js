27558295895d08fe724acceeb59d125c8dfa324a
jkest90 <jkest618@gmail.com>
2018-10-11 18:37:45 -0600

    location button added

--- App.js
+++ App.js
@@ -1,48 +1,50 @@
 import React from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
-import Map from './components/Map.js';
-import { Constants, Location, Permissions } from 'expo';
+import { Dimensions, Marker, Button, Platform, Text, View, StyleSheet } from 'react-native';
+// import Map from './components/Map.js';
+import { MapView, Constants, Location, Permissions } from 'expo';
 
 export default class App extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         location: '',
-         latitude: '',
-         longitude: '',
-         errorMessage: '',
+         location: {
+            coords: {
+               latitude: 37.78825,
+               longitude: -122.4324
+            }
+         },
+         locationResult: null,
+         errorMessage: ''
       }
    }
-   // state = {
-   //    location: '',
-   //    errorMessage: null,
+   //
+   // componentDidMount() {
+   //    this._getLocationAsync();
+   // }
 
-
-   componentWillMount() {
+   _handleMapRegionChange = mapRegion => {
       this._getLocationAsync();
-      if (Platform.OS === 'android' && !Constants.isDevice) {
-         this.setState({
-            errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device'
-         });
-      }
+      // this.setState({ mapRegion });
    }
 
    _getLocationAsync = async () => {
       let { status } = await Permissions.askAsync(Permissions.LOCATION);
       if (status !== 'granted') {
          this.setState({
-            errorMessage: 'Permission to access location was denied',
+            locationResult: 'Permission to access location was denied',
+            location
          });
       }
-
       let location = await Location.getCurrentPositionAsync({});
-      this.setState({
-         latitude: location.coords.latitude,
-         longitude: location.coords.longitude
-      });
+      this.setState({ locationResult: JSON.stringify(location), location });
+   }
+      // let location = await Location.getCurrentPositionAsync({});
+      // this.setState({
+      //    latitude: location.coords.latitude,
+      //    longitude: location.coords.longitude
+      // });
       // this.setState({ location.coords.})
 
-   }
 
    render() {
       // this._getLocationAsync();
@@ -56,7 +58,34 @@ export default class App extends React.Component {
 
       // console.log(text);
       return (
-         <Map latitude={this.state.latitude} longitude={this.state.longitude} />
+         <View style={styles.container}>
+            <MapView
+              style={styles.map}
+              region={{
+                 latitude: this.state.location.coords.latitude,
+                 longitude: this.state.location.coords.longitude,
+                 latitudeDelta: 0.0922,
+                 longitudeDelta: 0.0421
+              }}
+              // onRegionChange={this._handleMapChange}
+              showsPointsOfInterest={true}
+            >
+               <MapView.Marker
+                  coordinate={ this.state.location.coords }
+                  title={ 'myTitle' }
+                  description={ 'myDescription' }
+                  pinColor={ 'blue' }
+                  onCalloutPress={() => alert('Clicked')}
+                  >
+               </MapView.Marker>
+            </MapView>
+            <Button
+               onPress={this._handleMapRegionChange}
+               styles={styles.button}
+               title='Locate me!'
+               color='#841584'
+            />
+         </View>
     );
   }
 }
@@ -69,10 +98,20 @@ const styles = StyleSheet.create({
       paddingTop: Constants.statusBarHeight,
       backgroundColor: '#ecf0f1',
    },
+   button: {
+      position: 'absolute',
+   },
+   map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      zIndex: -1,
+   },
    paragraph: {
       margin: 24,
       fontSize: 18,
       textAlign: 'center',
    }
-
-})
+});
